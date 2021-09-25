bd9ed56fc85ecd36892ad13a68bf5f0c243397ed
bawdibuisreal <64428460+bawdibuisreal@users.noreply.github.com>
2021-09-25 15:36:17 +0700

    Update addmoney.js

--- commands/addmoney.js
+++ commands/addmoney.js
@@ -5,12 +5,12 @@ exports.execute = async (client, message, args) => {
     let user = message.mentions.users.first();
     if (!user) return message.channel.send("Please specify a user!");
     let amount = args[1];
-    if (!amount || isNaN(amount)) return message.reply("Please specify a valid amount.");
+    if (!amount || isNaN(amount)) return message.reply("xin hãy ghi số **Hoa** muốn tăng.");
     let data = client.eco.addMoney(user.id, parseInt(amount));
     const embed = new MessageEmbed()
         .setTitle(`Money Added!`)
         .addField(`User`, `<@${data.user}>`)
-        .addField(`Balance Given`, `${data.amount} 💸`)
+        .addField(`Số tiền`, `${data.amount} <:hoa:891221416958119936> `)
         .addField(`Total Amount`, data.after)
         .setColor("RANDOM")
         .setThumbnail(user.displayAvatarURL)
