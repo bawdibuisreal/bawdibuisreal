70e7abc75e512b478c3ede4425d6e3a83a8289b8
bawdibuisreal <64428460+bawdibuisreal@users.noreply.github.com>
2021-09-25 15:28:08 +0700

    Update setmoney.js

--- commands/setmoney.js
+++ commands/setmoney.js
@@ -3,9 +3,9 @@ const { MessageEmbed } = require("discord.js");
 exports.execute = async (client, message, args) => {
     if (!client.config.admins.includes(message.author.id)) return; // return if author isn't bot owner
     let user = message.mentions.users.first();
-    if (!user) return message.channel.send("Please specify a user!");
+    if (!user) return message.channel.send("Xin Hãy Ping!");
     let amount = args[1];
-    if (!amount || isNaN(amount)) return message.reply("Please specify a valid amount.");
+    if (!amount || isNaN(amount)) return message.reply("Xin hãy ghi số **Hoa**.");
     let data = client.eco.setMoney(user.id, parseInt(amount));
     const embed = new MessageEmbed()
         .setTitle(`Money Updated!`)
