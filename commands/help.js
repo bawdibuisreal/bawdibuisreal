d5f382e100c540c72c3d2e409e6a1defc55bebe9
bawdibuisreal <64428460+bawdibuisreal@users.noreply.github.com>
2021-09-25 15:25:24 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,10 +2,8 @@ const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
     const embed = new MessageEmbed()
-        .setAuthor("Commands")
-        .setTitle("Make Sure To Check Out This Channel For More Such Bots")
-        .setURL("https://www.youtube.com/channel/UCF9E-xef9jL9QgziZRDHKKQ")
-        .setDescription(`Total Commands: ${client.commands.size}`)
+        .setAuthor("HELP")
+        .setDescription(`Số những câu lệnh: ${client.commands.size}`)
         .setColor("BLURPLE")
         .setTimestamp()
         .setThumbnail(client.user.displayAvatarURL)
