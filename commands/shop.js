a3acbd530ac194387ca4fdab1264e9eac88dade3
bawdibuisreal <64428460+bawdibuisreal@users.noreply.github.com>
2021-09-26 15:56:40 +0700

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -10,8 +10,13 @@ exports.execute = async (client, message, args) => {
   
   let embed = new MessageEmbed()
   .setTitle("Store")
-  .setDescription(content)
-  .setColor("BLURPLE")
+ .setTitle('Bấm vào đây để mua')
+ .setURL('https://discord.gg/292ZqPrubj')
+ .setDescription(Bán Nitro classic với giá 4m2<:emoji_40:891605292788371488>
+  Bán nitro Boost với giá 9m2<:emoji_40:891605292788371488>
+  Bán acc netflix giá 2m<:emoji_40:891605292788371488>
+  BÁN Spotify 4m<:emoji_40:891605292788371488> Nếu mua năm thì dms giá)
+.setColor("PINK")
   .setFooter("Do :?buy <item> to purchase the item.")
   return message.channel.send(embed);
 };
