d5772edeb6dc3e61a03bc8b588fcbed2eaacad23
bawdibuisreal <64428460+bawdibuisreal@users.noreply.github.com>
2021-09-26 15:58:42 +0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -2,7 +2,7 @@ module.exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 500) + 100;
     let addMoney = client.eco.daily(client.ecoAddUser, amount);
     if (addMoney.onCooldown) return message.reply(`Bạn đã nhận số **Hoa** ngày hôm nay,hãy đợi thêm ${addMoney.time.hours} giờ, ${addMoney.time.minutes} phút & ${addMoney.time.seconds} giây để nhận thêm.`);
-    else return message.reply(`Bạn đã nhận được **${addMoney.amount}** <:hoa:891221416958119936>.`);
+    else return message.reply(`Bạn đã nhận được **${addMoney.amount}** <:emoji_40:891605292788371488>.`);
 };
 
 module.exports.help = {
