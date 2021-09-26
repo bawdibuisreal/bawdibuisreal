a1037b2004f2e0fcb9079aa3bc30628ec0e36767
bawdibuisreal <64428460+bawdibuisreal@users.noreply.github.com>
2021-09-26 15:59:47 +0700

    Update weekly.js

--- commands/weekly.js
+++ commands/weekly.js
@@ -2,7 +2,7 @@ exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 1000) + 500;
     let addMoney = client.eco.weekly(client.ecoAddUser, amount);
     if (addMoney.onCooldown) return message.reply(`Bạn đã nhận số **Hoa** ngày hôm nay,hãy đợi thêm ${addMoney.time.days} Ngày, ${addMoney.time.hours} giờ, ${addMoney.time.minutes} phút & ${addMoney.time.seconds} giây để nhận lần nữa`);
-    else return message.reply(`Bạn đã nhận được **${addMoney.amount}** <:hoa:891221416958119936> :P`);
+    else return message.reply(`Bạn đã nhận được **${addMoney.amount}** <:emoji_40:891605292788371488> :P`);
 };
 
 exports.help = {
