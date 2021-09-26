f778afdc33fc05816f3530ecef549dcfa9687b4c
bawdibuisreal <64428460+bawdibuisreal@users.noreply.github.com>
2021-09-26 15:44:55 +0700

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -1,8 +1,8 @@
 module.exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 20) + 5;
     let work = client.eco.work(client.ecoAddUser, amount);
-    if (work.onCooldown) return message.reply(`Bạn hãy đợi thêm ${work.time.minutes} phút & ${work.time.seconds} giây để trông **Hoa** lần nữa.`);
-    else return message.reply(`Bạn đã trông hoa chăm chỉ và nhận được **${work.amount}** <:hoa:891221416958119936>.`);
+    if (work.onCooldown) return message.reply(`Bạn hãy đợi thêm ${work.time.minutes} phút & ${work.time.seconds} giây để trồng **Hoa** lần nữa.`);
+    else return message.reply(`Bạn đã trồng hoa chăm chỉ và nhận được **${work.amount}** <:emoji_40:891605292788371488>.`);
 };
 
 module.exports.help = {
